e30d12f3c5cf4ed0478454718fc9269a8899faac
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-28 15:49:08 +0530

    Notification component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
+import React from "react";
+import Notification from "./components/UI/Notification";
 
 function App() {
+  
+  const dispatch = useDispatch();
 
-  const showCart = useSelector((state) => state.ui.cartIsVisible)
-  
-  
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+
+  const notification = useSelector(state => state.ui.notification);
+  console.log(notification)
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending",
+          message: "Sending Cart Data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-242c6-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent Cart Data SuccessFully!",
+        })
+      );
+    };
+
+
+    sendCartData().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending Cart Data Failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <React.Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </React.Fragment>
   );
 }
 
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,20 +1,26 @@
+import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-import { useDispatch } from 'react-redux';
-import { cartActions } from '../../store/cart-slice';
-
 
 const ProductItem = (props) => {
+  const dispatch = useDispatch();
+
   const { title, price, description, id } = props;
-  const dispatch = useDispatch()
 
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({
-      id: id,
-      title: title,
-      price: price
-    }))
-  }
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
