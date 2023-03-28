953432f953716b8d05bc024e42588158f64a96e3
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-28 13:58:10 +0530

    cart using redux

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import store from './store/index2';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Provider store={store}><App /></Provider>);
